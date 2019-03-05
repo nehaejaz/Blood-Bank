9b301a0cf335d7f414c2a271aa6809d179bf6b83
nehaejaz <neha.ejaz@icloud.com>
2019-03-05 05:08:26 +0500

    upgraded

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -1,18 +1,32 @@
-import React from 'react'
+import React,{Component} from 'react'
 import {Link} from 'react-router-dom'
-import {Icon,Dropdown,NavItem,Button} from 'react-materialize'
+import {Icon,Dropdown,NavItem,Button,SideNav,SideNavItem} from 'react-materialize'
+import Notification from '../dashboard/Notification'
 
-const SignedInLinks = () => {
+const SignedInLinks =()=>{
+ 
+ 
     return(
         <div className='SignedInLinks'>
          <ul id="nav-mobile"className="right hide-on-med-and-down">
-         <li><Dropdown alignmet='right' trigger={
-    <a class="btn-floating disabled"><i class="material-icons">notifications</i></a>
-  }>
-  <NavItem>one</NavItem>
-  <NavItem divider />
-  <NavItem>two</NavItem>
-</Dropdown></li>
+       <li>  <SideNav
+  trigger={<a class="btn-floating grey darken-3" ><i class="material-icons">notifications</i></a>}
+  options={{ closeOnClick: true,edge: 'right' }}
+  >
+  <SideNavItem userView
+    user={{
+      background: 'img/office.jpg',
+      image: 'img/yuna.jpg',
+      name: 'John Doe',
+      email: 'jdandturk@gmail.com'
+    }}
+  />
+  <SideNavItem href='#!icon' icon='cloud'>First Link With Icon</SideNavItem>
+  <SideNavItem href='#!second'>Second Link</SideNavItem>
+  <SideNavItem divider />
+  <SideNavItem subheader>Subheader</SideNavItem>
+  <SideNavItem waves href='#!third'>Third Link With Waves</SideNavItem>
+</SideNav></li>
         <li><Link to='/registerdonor'>Register Donor </Link></li>
         <li><Link to='/'>Logout</Link></li>
         <li><Link to='/' className='btn btn-floating red'>NN</Link></li>
