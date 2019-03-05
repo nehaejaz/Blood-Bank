de246635bad8f52e9df482658ccb3516559a101f
nehaejaz <neha.ejaz@icloud.com>
2019-03-05 05:09:22 +0500

    auth reducer

---
+++ src/store/reducers/authReducer.js
@@ -0,0 +1,15 @@
+// const initState = {
+
+// }
+
+// const authReducer = (state=initState,action) =>{
+//     switch(action.type){
+//         case 'Sign_Up':
+//         console.log("user Signed up")
+
+//         case 'Login':
+//         console.log('user logged in')
+//     }
+// }
+
+// export default authReducer
